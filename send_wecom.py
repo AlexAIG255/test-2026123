d8024c6312bc5fa9c65cafea15d814cb83b7bc41
AlexAIG255 <alexkong255123@gmail.com>
2026-06-16 15:39:59 +0800

    Update send_wecom.py

--- send_wecom.py
+++ send_wecom.py
@@ -1,9 +1,25 @@
 import os
 
+import requests
+
 webhook = os.getenv("WECOM_WEBHOOK")
 
 print("Webhook =", webhook)
 
-if webhook is None:
+msg = {
 
-    raise Exception("Secret Empty")
+    "msgtype": "text",
+
+    "text": {
+
+        "content": "AI Stock Agent 测试消息"
+
+    }
+
+}
+
+r = requests.post(webhook, json=msg)
+
+print("Status Code:", r.status_code)
+
+print("Response:", r.text)
